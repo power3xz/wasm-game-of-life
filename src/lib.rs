aa7b881832ed597362243db78f2b6c25f32be493
devj <power3xz@gmail.com>
2022-10-16 23:43:38 +0900

    Universe 초기화 함수 추가

--- src/lib.rs
+++ src/lib.rs
@@ -70,6 +70,36 @@ impl Universe {
             }
         }
     }
+
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl Default for Universe {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl fmt::Display for Universe {
