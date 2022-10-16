b8e2fbb3e12525a9fa62dc3c91863e1bd1bbe96a
devj <power3xz@gmail.com>
2022-10-17 00:31:39 +0900

    메모리에 접근하여 렌더링

--- www/src/main.ts
+++ www/src/main.ts
@@ -1,11 +1,77 @@
-import { Universe } from "wasm-game-of-life";
+import { Cell, Universe } from "wasm-game-of-life";
+import { memory } from "wasm-game-of-life/wasm_game_of_life_bg.wasm";
 
-const pre = document.getElementById("game-of-life-canvas")!;
+const CELL_SIZE = 5;
+const GRID_COLOR = "#ccc";
+const DEAD_COLOR = "#fff";
+const ALIVE_COLOR = "#000";
+
 const universe = Universe.new();
-console.log(universe);
+const width = universe.width();
+const height = universe.height();
+const canvas = document.getElementById(
+  "game-of-life-canvas"
+)! as HTMLCanvasElement;
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+const ctx = canvas.getContext("2d");
+
+const drawGrid = () => {
+  if (!ctx) {
+    return;
+  }
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  for (let i = 0; i <= width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  for (let j = 0; j <= height; j++) {
+    ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+  }
+  ctx.stroke();
+};
+
+const getIndex = (row: number, column: number) => {
+  return row * width + column;
+};
+
+const drawCells = () => {
+  if (!ctx) {
+    return;
+  }
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      ctx.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  ctx.stroke();
+};
+
 const renderLoop = () => {
-  pre.textContent = universe.render();
   universe.tick();
+
+  drawGrid();
+  drawCells();
+
   requestAnimationFrame(renderLoop);
 };
 
