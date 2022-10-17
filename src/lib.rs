8686f795a28c82b485aed59db64ab5aa2980b806
devj <power3xz@gmail.com>
2022-10-17 10:16:57 +0900

    Cell 구조체 제거

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,6 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-#[repr(u8)]
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Cell {
-    Dead = 0,
-    Alive = 1,
-}
-
-#[wasm_bindgen]
 pub struct Universe {
     width: u32,
     height: u32,
